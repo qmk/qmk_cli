97689b53d7d31909347a7b599eeca3af5a5e5db3
zvecr <git@zvecr.com>
2021-04-05 21:29:56 +0100

    Add cli.config.general

--- qmk_cli/subcommands/env.py
+++ qmk_cli/subcommands/env.py
@@ -9,13 +9,18 @@ from milc import cli
 @cli.subcommand('Prints environment information.')
 def env(cli):
     data = {
-        'QMK_HOME' : os.environ.get('QMK_HOME', "")
+        'QMK_HOME': os.environ.get('QMK_HOME', "")
     }
+ 
+    # Now munge the current cli config
+    for key, val in cli.config.general.items():
+        converted_key = 'QMK_' + key.upper()
+        data[converted_key] = val
 
     if cli.args.var:
         # dump out requested arg
         print(data[cli.args.var])
     else:
         # dump out everything
-        for key,val in data.items():
+        for key, val in data.items():
             print(f'{key}="{val}"')
