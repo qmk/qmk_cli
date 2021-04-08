cbe3ad0248544522e3c4f067239a6b81994b7371
zvecr <git@zvecr.com>
2021-04-08 23:41:12 +0100

    Expose is_qmk_firmware status

--- qmk_cli/subcommands/env.py
+++ qmk_cli/subcommands/env.py
@@ -1,17 +1,21 @@
 """Prints environment information.
 """
 import os
+from pathlib import Path
 
 from milc import cli
+from qmk_cli.helpers import is_qmk_firmware
 
 
 @cli.argument('var', default=None, nargs='?', help='Optional variable to query')
 @cli.subcommand('Prints environment information.')
 def env(cli):
+    home = os.environ.get('QMK_HOME', "")
     data = {
-        'QMK_HOME': os.environ.get('QMK_HOME', "")
+        'QMK_HOME': home,
+        'QMK_FIRMWARE': home if is_qmk_firmware(Path(home)) else ""
     }
- 
+
     # Now munge the current cli config
     for key, val in cli.config.general.items():
         converted_key = 'QMK_' + key.upper()
